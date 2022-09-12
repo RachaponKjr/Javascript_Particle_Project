1e53b98b3e119835ffdf7b020e2d7f7804f78611
RachaponKjr <rachapon.navy@gmail.com>
2022-09-12 13:18:55 +0700

    Update client.ts

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,11 +2,11 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 
-console.log(Math.random())
-
-const cube_list = []
-
 const scene = new THREE.Scene()
+const cubelist = []
+const axesHelp = new THREE.AxesHelper(4)
+
+
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -15,11 +15,14 @@ const camera = new THREE.PerspectiveCamera(
     1000
 )
 
+
+//light list
 const light = new THREE.AmbientLight()
 light.position.z = 5
 light.position.x = 10
-scene.add(light)
 
+const lightPoint = new THREE.PointLight(0xffffff, 1 , 100)
+lightPoint.position.set(0,10,5)
 
 camera.position.z = 2
 
@@ -37,7 +40,9 @@ for(let i = 0; i<1000; i++){
 
     const geometry = new THREE.DodecahedronGeometry( Math.random()/4 + 0.1)
     const material = new THREE.MeshMatcapMaterial({
-    color: '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')
+    color: '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'),
+    transparent : true,
+    opacity : 0.4
 
 })
     
@@ -48,15 +53,21 @@ var cube = new THREE.Mesh(geometry, material)
 cube.position.x = Math.random() * 10 - 5
 cube.position.y = Math.random() * 10 - 5
 cube.position.z = Math.random() * 10 - 5
+cube.receiveShadow = true;
+
 
 scene.add(cube)
-cube_list.push(cube)
+cubelist.push(cube)
+
+
 
 
 }
 
 
 window.addEventListener('resize', onWindowResize, false)
+
+
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
@@ -74,5 +85,11 @@ function animate() {
 function render() {
     renderer.render(scene, camera)
 }
+
+
+//scens add
+scene.add(axesHelp)
+scene.add(light)
+scene.add(lightPoint)
 
 animate()
